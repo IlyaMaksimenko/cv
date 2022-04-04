80dc98582756d709e8ec79644a2309bce9da53a5
ilya.maksimenko <imax1287@gmail.com>
2022-04-04 18:41:09 +0300

    added hello dialog

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
+  <v-app>
+    <HelloDialog
+      :visible="isHelloDialogVisible"
+      name="username"
+      @update:visible="updateHelloDialogVisible"
     />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+
+    <v-main v-if="!showHi">
+      <v-container fluid>
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import HelloDialog from "./components/HelloDialog.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
+    HelloDialog,
     HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  isHelloDialogVisible = false;
+
+  updateHelloDialogVisible(value: boolean) {
+    this.isHelloDialogVisible = value;
+  }
+
+  mounted() {
+    this.isHelloDialogVisible = true;
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  overflow-y: auto !important; // cause - https://github.com/vuetifyjs/vuetify/issues/1197;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/HelloDialog.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-dialog
+    v-model="syncedVisible"
+    fullscreen
+    hide-overlay
+    transition="dialog-top-transition"
+  >
+    <div
+      class="hello-dialog-content"
+      @wheel.prevent.stop="onWheel"
+      @touchmove.prevent.stop="onTouchMove"
+    >
+      <h1>Hello!</h1>
+      <h2>My name is {{ name }}!</h2>
+      <v-btn
+        @click="hide"
+        class="my-4"
+      >
+        Hi!
+      </v-btn>
+    </div>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Prop, PropSync, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloDialog extends Vue {
+  @PropSync("visible", { type: Boolean }) syncedVisible!: boolean;
+  @Prop() name!: string;
+
+  hide() {
+    this.syncedVisible = false;
+  }
+
+  onTouchMove() {
+    this.hide();
+  }
+
+  onWheel() {
+    this.hide();
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  scoped
+>
+.hello-dialog-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+
+  h1,
+  h2,
+  h3 {
+    font-weight: 300;
+  }
+
+  button {
+    text-transform: none;
+  }
+}
+</style>
